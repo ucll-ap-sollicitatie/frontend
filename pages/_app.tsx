99ca7f7747eebfc181bc67b668328eee604e345d
Lorenzo Catalano <lorenzocatalano37@gmail.com>
2022-02-14 12:06:33 +0100

    Added bootstrap

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css';
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap");
+}, []);
+
   return <Component {...pageProps} />;
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const Home: NextPage = () => {
   return (
