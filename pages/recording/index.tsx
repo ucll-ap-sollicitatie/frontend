96ddb7a19571fcdaf9a158543d8cc27055976dc9
Maarten-VanBriel <maarten.vanbriel@student.ucll.be>
2022-02-16 10:23:05 +0100

    record video v1

--- pages/recording/index.tsx
+++ pages/recording/index.tsx
@@ -1,138 +1,79 @@
 import type { NextPage } from "next";
 import React from "react";
-import {
-  RecordWebcam,
-  useRecordWebcam,
-  CAMERA_STATUS
-} from "react-record-webcam";
-import type {
-  WebcamRenderProps,
-  RecordWebcamOptions,
-  RecordWebcamHook
-} from "react-record-webcam";
+import Webcam from "react-webcam";
 import Link from "next/link";
 import Layout from "../../components/layout/Layout";
 
-const OPTIONS: RecordWebcamOptions = {
-    filename: "test-filename",
-    fileType: "mp4",
-    width: 500,
-    height: 300
+const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user"
   };
+
 
 const Recording: NextPage = () => {
-  const recordWebcam: RecordWebcamHook = useRecordWebcam(OPTIONS);
+    const webcamRef = React.useRef(null);
+    const mediaRecorderRef = React.useRef(null);
+    const [capturing, setCapturing] = React.useState(false);
+    const [recordedChunks, setRecordedChunks] = React.useState([]);
+  
+    const handleStartCaptureClick = React.useCallback(() => {
+      setCapturing(true);
+      mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+        mimeType: "video/webm"
+      });
+      mediaRecorderRef.current.addEventListener(
+        "dataavailable",
+        handleDataAvailable
+      );
+      mediaRecorderRef.current.start();
+    }, [webcamRef, setCapturing, mediaRecorderRef]);
+  
+    const handleDataAvailable = React.useCallback(
+      ({ data }) => {
+        if (data.size > 0) {
+          setRecordedChunks((prev) => prev.concat(data));
+        }
+      },
+      [setRecordedChunks]
+    );
+  
+    const handleStopCaptureClick = React.useCallback(() => {
+      mediaRecorderRef.current.stop();
+      setCapturing(false);
+    }, [mediaRecorderRef, webcamRef, setCapturing]);
+  
+    const handleDownload = React.useCallback(() => {
+      if (recordedChunks.length) {
+        const blob = new Blob(recordedChunks, {
+          type: "video/webm"
+        });
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        a.href = url;
+        a.download = "react-webcam-stream-capture.webm";
+        a.click();
+        window.URL.revokeObjectURL(url);
+        setRecordedChunks([]);
+      }
+    }, [recordedChunks]);
 
-  //var previewVideo = document.getElementById("previewVideo");
-
-  function playPreviewVideo() {
-    //previewVideo.play();
-  };
-
-  function pausePreviewVideo() {
-    //previewVideo.pause();
-  };
 
   return (
     <>
       <Layout>
         <h1>Slim op sollicitatie</h1>
-        <div className="demo-section">
-            <h1>Hooks demo</h1>
-            <video id="recordingVideo"
-            ref={recordWebcam.webcamRef}
-            style={{
-                display: `${
-                recordWebcam.status === CAMERA_STATUS.OPEN ||
-                recordWebcam.status === CAMERA_STATUS.RECORDING
-                    ? "block"
-                    : "none"
-                }`
-            }}
-            autoPlay
-            muted
-            />
-            <video id="previewVideo"
-            ref={recordWebcam.previewRef}
-            style={{
-                display: `${
-                    recordWebcam.status === CAMERA_STATUS.PREVIEW ? "block" : "none"
-                }`
-            }}
-            
-            />
-            <div>
-            <button
-                className="btn btn-primary"
-                hidden={
-                recordWebcam.status === CAMERA_STATUS.OPEN ||
-                recordWebcam.status === CAMERA_STATUS.RECORDING ||
-                recordWebcam.status === CAMERA_STATUS.PREVIEW
-                }
-                onClick={recordWebcam.open}
-            >
-                Open camera
-            </button>
-            <button
-                hidden={
-                recordWebcam.status === CAMERA_STATUS.CLOSED ||
-                recordWebcam.status === CAMERA_STATUS.PREVIEW
-                }
-                onClick={recordWebcam.close}
-                className="btn btn-primary"
-            >
-                Close camera
-            </button>
-            <button
-                hidden={
-                recordWebcam.status === CAMERA_STATUS.CLOSED ||
-                recordWebcam.status === CAMERA_STATUS.RECORDING ||
-                recordWebcam.status === CAMERA_STATUS.PREVIEW
-                }
-                onClick={recordWebcam.start}
-                className="btn btn-primary"
-            >
-                Start recording
-            </button>
-            <button
-                hidden={recordWebcam.status !== CAMERA_STATUS.RECORDING}
-                onClick={recordWebcam.stop}
-                className="btn btn-primary"
-            >
-                Stop recording
-            </button>
-            <button
-                hidden={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
-                onClick={recordWebcam.retake}
-                className="btn btn-primary"
-            >
-                Retake
-            </button>
-            <button
-                hidden={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
-                onClick={recordWebcam.download}
-                className="btn btn-primary"
-            >
-                Download
-            </button>
-            <button
-                id="PlayPreview"
-                hidden={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
-                onClick={() => playPreviewVideo()}
-                className="btn btn-primary"
-            >
-                Play Preview
-            </button>
-            <button
-                id="PausePreview"
-                hidden={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
-                onClick={() => pausePreviewVideo()}
-                className="btn btn-primary"
-            >
-                Pause Preview
-            </button>
-            </div>
-        </div>
+        <Webcam audio={true} ref={webcamRef} muted/>
+        {capturing ? (
+            <button onClick={handleStopCaptureClick}>Stop Recording</button>
+        ) : (
+            <button onClick={handleStartCaptureClick}>Start Recording</button>
+        )}
+        {recordedChunks.length > 0 && (
+            <button onClick={handleDownload}>Download</button>
+        )}
       </Layout>
     </>
   );
