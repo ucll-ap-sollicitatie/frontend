f31ea4bc75fa9ef5081ae01faf6c93bca855892e
Maarten-VanBriel <maarten.vanbriel@student.ucll.be>
2022-02-18 12:26:32 +0100

    werkt niet

--- components/layout/Upload.tsx
+++ components/layout/Upload.tsx
@@ -4,7 +4,7 @@ export function Upload(recordedChunks: any & never[]) {
     const [publicId, setPublicId] = useState("");
     const onChange = async (event: { preventDefault: () => void; target: { files: any[]; }; }) => {
     event.preventDefault();
-
+    console.log(recordedChunks)
     const blob = new Blob(recordedChunks[-1], {
         type: "video/webm"
     });
@@ -12,7 +12,6 @@ export function Upload(recordedChunks: any & never[]) {
     const formData = new FormData();
     const file = url;
     formData.append("inputFile", file);
-
     try {
       const response = await fetch("http://localhost:3001/video-uploading", {
         method: "POST",
