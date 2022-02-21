c62d53639ec39e9d551c6044a56d1038d011dbba
ghostek99 <szymon.nidecki@student.ucll.be>
2022-02-21 16:52:03 +0100

    added description to videos

--- interfaces/Video.tsx
+++ interfaces/Video.tsx
@@ -4,4 +4,5 @@ export interface Video {
   date: Date;
   r_u_number: string;
   email: string;
+  description: string;
 }
--- pages/videos/[id].tsx
+++ pages/videos/[id].tsx
@@ -1,9 +1,10 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useSession } from "next-auth/react";
+import { Video } from "../../interfaces/Video";
+import { Col, Container, Row } from "react-bootstrap";
 import Layout from "../../components/layout/Layout";
 import Unauthenticated from "../../components/Unauthenticated";
 import VideoPlayer from "../../components/VideoPlayer";
-import { Video } from "../../interfaces/Video";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const data = await fetch("http://localhost:3001/videos/");
@@ -50,8 +51,18 @@ const Video: NextPage<Props> = ({ video }) => {
   console.table({ userEmail, videoTitle });
   return (
     <Layout>
-      <h1>{video.title}</h1>
-      <VideoPlayer userEmail={userEmail} videoTitle={videoTitle} />
+      <Container>
+        <Row>
+          <Col>
+            <h1>{video.title}</h1>
+            <VideoPlayer userEmail={userEmail} videoTitle={videoTitle}></VideoPlayer>
+          </Col>
+          <Col>
+            <h1>Omschrijving</h1>
+            <p>{video.description}</p>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   );
 };
