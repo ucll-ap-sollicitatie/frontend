0ac3f6d4f5243d9b6342d7c93f7d2525b637db8d
ghostek99 <szymon.nidecki@student.ucll.be>
2022-02-22 17:28:48 +0100

    comemnts now have options

--- helpers/helperFunctions.tsx
+++ helpers/helperFunctions.tsx
@@ -3,3 +3,29 @@ export function capitalize(word: string): string {
 
   return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
+
+export function timeSince(date: Date): string {
+  const seconds = Math.floor((new Date().valueOf() - new Date(date).valueOf()) / 1000);
+  let interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " jaar geleden";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " maand geleden";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " dagen geleden";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " uur geleden";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minuten geleden";
+  }
+  return Math.floor(seconds) + " seconden geleden";
+}
