59d87b62199079a7919004e7c76fc3dc00f6f132
Lorenzo Catalano <lorenzocatalano37@gmail.com>
2022-02-28 20:34:40 +0100

    Recording translated

--- components/recording/CarouselWithQuestions.tsx
+++ components/recording/CarouselWithQuestions.tsx
@@ -1,4 +1,5 @@
 import { NextPage } from "next";
+import { useTranslations } from "next-intl";
 import { Carousel, Image } from "react-bootstrap";
 import { Question } from "../../interfaces/Question";
 
@@ -8,13 +9,17 @@ interface Props {
 }
 
 const CarouselWithQuestions: NextPage<Props> = ({ handleSelect, questions }) => {
+  const t = useTranslations("recording");
+
   return (
     <Carousel onSelect={handleSelect} interval={null} variant="dark" wrap={false}>
       {questions.map((question, index) => (
         <Carousel.Item key={index}>
           <Image className="d-block w-50" src="https://via.placeholder.com/600x400/ffffff/ffffff" alt="Carousel slide" />
           <Carousel.Caption>
-            <h2>Vraag {index + 1}</h2>
+            <h2>
+              {t("question")} {index + 1}
+            </h2>
             <h3>{question.question}</h3>
           </Carousel.Caption>
         </Carousel.Item>
