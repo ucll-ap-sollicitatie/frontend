bd1e47ac924854bdaa9b8b2d74c563b320024bf2
ghostek99 <szymon.nidecki@student.ucll.be>
2022-02-28 13:27:38 +0100

    recording layout

---
+++ components/recording/CarouselWithQuestions.tsx
@@ -0,0 +1,26 @@
+import { NextPage } from "next";
+import { Carousel } from "react-bootstrap";
+import { Question } from "../../interfaces/Question";
+
+interface Props {
+  handleSelect: Function;
+  questions: Question[];
+}
+
+const CarouselWithQuestions: NextPage<Props> = ({ handleSelect, questions }) => {
+  return (
+    <Carousel onSelect={handleSelect} interval={null} variant="dark" wrap={false}>
+      {questions.map((question, index) => (
+        <Carousel.Item key={index}>
+          <img className="d-block w-50" src="https://via.placeholder.com/600x400/ffffff/ffffff" alt="Carousel slide" />
+          <Carousel.Caption>
+            <h2>Vraag {index + 1}</h2>
+            <h3>{question.question}</h3>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
+    </Carousel>
+  );
+};
+
+export default CarouselWithQuestions;
---
+++ components/recording/RecordingUploadForm.tsx
@@ -0,0 +1,62 @@
+import { NextPage } from "next";
+import error from "next/error";
+import { Form, OverlayTrigger, Tooltip, Button, Spinner, Alert } from "react-bootstrap";
+
+interface Props {
+  handleUpload: Function;
+  setMaxChars: Function;
+  maxChars: number;
+  handleBackClick: Function;
+  setUploading: Function;
+  uploading: boolean;
+}
+
+const RecordingUploadForm: NextPage<Props> = ({ handleUpload, setMaxChars, maxChars, handleBackClick, setUploading, uploading }) => {
+  return (
+    <Form onSubmit={handleUpload} className="col-md-12 col-lg-10 col-xl-8">
+      <div className="d-flex gap-4 flex-wrap">
+        <Form.Group controlId="title">
+          <Form.Label>Titel:</Form.Label>
+          <Form.Control type="text" placeholder="e.g. Mijn interviewopname" required />
+          <Form.Text className="text-muted">Titel mag geen vraagtekens (?) of hashtags (#) bevatten</Form.Text>
+        </Form.Group>
+      </div>
+      <div className="gap-4 flex-wrap mt-2">
+        <Form.Group controlId="description">
+          <Form.Label>Omschrijving:</Form.Label>
+          <Form.Control
+            onChange={(e) => setMaxChars(e.target.value.length)}
+            as="textarea"
+            rows={4}
+            maxLength={255}
+            placeholder="e.g. Mijn interview voor back-end web developer"
+          />
+          <Form.Text className="text-muted">Karakters: {255 - maxChars}/255</Form.Text>
+        </Form.Group>
+      </div>
+      <div className="d-flex gap-4 flex-wrap">
+        <OverlayTrigger placement="top" overlay={<Tooltip id="button-tooltip-2">Privé: alleen lectoren kunnen jouw video zien</Tooltip>}>
+          <Form.Group className="mb-3" controlId="privateCheckbox">
+            <Form.Check type="checkbox" label="Privé" defaultChecked />
+          </Form.Group>
+        </OverlayTrigger>
+      </div>
+      {!uploading && (
+        <Button variant="primary" type="submit" className="mt-3">
+          Uploaden
+        </Button>
+      )}
+      {uploading && (
+        <Button variant="primary" className="mt-3" disabled>
+          <Spinner as="span" animation="grow" size="sm" role="status" aria-hidden="true" />
+          Uploading...
+        </Button>
+      )}
+      <Button variant="light" className="mt-3 ms-2" onClick={handleBackClick}>
+        Terug
+      </Button>
+    </Form>
+  );
+};
+
+export default RecordingUploadForm;
