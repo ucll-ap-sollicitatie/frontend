aa091411457330d86f24d7a9c7d22fc91f587017
Lorenzo Catalano <lorenzocatalano37@gmail.com>
2022-03-01 11:28:49 +0100

    Video search #MaartenBoosted

--- components/videos/AllVideoOverview.tsx
+++ components/videos/AllVideoOverview.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
-import { Row } from "react-bootstrap";
+import { ChangeEvent, useState } from "react";
+import { Form, Row } from "react-bootstrap";
 import User from "../../interfaces/User";
 import Video from "../../interfaces/Video";
 import Unauthenticated from "../Unauthenticated";
@@ -23,21 +24,38 @@ interface Props {
 const AllVideoOverview: NextPage<Props> = ({ videos, user }) => {
   const t = useTranslations("videos");
 
+  const [videosFiltered, setVideosFiltered] = useState(videos);
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const search = e.currentTarget.value;
+    const videosFiltered = videos.filter(
+      (video) => video.title.toLowerCase().includes(search.toLowerCase()) || video.email.toLowerCase().includes(search.toLowerCase())
+    );
+    setVideosFiltered(videosFiltered);
+  };
+
   const { data: session } = useSession();
   if (!session) return <Unauthenticated />;
   if (!videos) return <p>{t("no_videos")}</p>;
 
   return (
-    <Row className="g-4">
-      {videos.map(
-        (video: Video) =>
-          (!video.private || user.role != "Student") && (
-            <div className="col-md-6 col-lg-4 col-xl-3" key={video.video_id}>
-              <VideoCard video={video} />
-            </div>
-          )
-      )}
-    </Row>
+    <>
+      <Form.Group controlId="search">
+        <Form.Label>{t("search")}</Form.Label>
+        <Form.Control onChange={handleSearch} type="text" placeholder={t("search")} required />
+      </Form.Group>
+      <br />
+
+      <Row className="g-4">
+        {videosFiltered.map(
+          (video: Video) =>
+            (!video.private || user.role != "Student") && (
+              <div className="col-md-6 col-lg-4 col-xl-3" key={video.video_id}>
+                <VideoCard video={video} />
+              </div>
+            )
+        )}
+      </Row>
+    </>
   );
 };
 
