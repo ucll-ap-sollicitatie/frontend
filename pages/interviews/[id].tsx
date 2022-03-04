efff9c3e77fe4dc7c151693d27ea4f44fab2d40f
ghostek99 <szymon.nidecki@student.ucll.be>
2022-03-04 21:16:20 +0100

    test

--- pages/interviews/[id].tsx
+++ pages/interviews/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useTranslations } from "next-intl";
 import { Carousel } from "react-bootstrap";
@@ -34,7 +34,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   };
 };
 
-export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+export const getServerSideProps: GetServerSideProps = async ({ params, locale }) => {
   if (params === undefined || params.id === undefined) {
     return { props: { questions: [], category: null } };
   }
@@ -54,9 +54,33 @@ export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
       category: category,
       messages: (await import(`../../public/locales/${locale}.json`)).default,
     },
-    revalidate: 5,
+    // revalidate: 5,
   };
 };
+
+// export const getStaticProps: GetStaticProps = async ({ params, locale }) => {
+//   if (params === undefined || params.id === undefined) {
+//     return { props: { questions: [], category: null } };
+//   }
+
+//   const category_res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/question-categories/${params.id}`);
+//   const category = await category_res.json();
+
+//   const questions_res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/questions/category/${params.id}`);
+//   if (questions_res.status !== 200) {
+//     return { props: { questions: [], category: category } };
+//   }
+//   const questions = await questions_res.json();
+
+//   return {
+//     props: {
+//       questions: questions,
+//       category: category,
+//       messages: (await import(`../../public/locales/${locale}.json`)).default,
+//     },
+//     revalidate: 5,
+//   };
+// };
 
 interface Props {
   questions: Question[];
