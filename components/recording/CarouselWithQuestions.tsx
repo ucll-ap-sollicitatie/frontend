f399973001fd1fb657d13f3d56faf5dfc46e22d5
ghostek99 <szymon.nidecki@student.ucll.be>
2022-03-11 19:06:00 +0100

    final fix?

--- components/recording/CarouselWithQuestions.tsx
+++ components/recording/CarouselWithQuestions.tsx
@@ -4,6 +4,8 @@ import { Carousel, Image } from "react-bootstrap";
 import { isDarkMode } from "../../helpers/helperFunctions";
 import Question from "../../interfaces/Question";
 
+// import { Carousel } from "react-responsive-carousel";
+
 interface Props {
   handleSelect?: (selectedIndex: number) => void | undefined;
   questions: Question[];
@@ -13,14 +15,25 @@ const CarouselWithQuestions: NextPage<Props> = ({ handleSelect, questions }) => 
   const t = useTranslations("carousel");
 
   return (
+    // <Carousel infiniteLoop={false} interval={0} onChange={handleSelect} swipeable={false} dynamicHeight={true}>
+    //   <div>
+    //     <p className="legend">Legend 1</p>
+    //   </div>
+    //   <div>
+    //     <p className="legend">Legend 2</p>
+    //   </div>
+    //   <div>
+    //     <p className="legend">Legend 3</p>
+    //   </div>
+    // </Carousel>
     <Carousel onSelect={handleSelect} interval={null} variant="dark" wrap={false}>
       {questions.map((question, index) => (
         <Carousel.Item key={index} style={{ minHeight: "320px" }}>
-          {isDarkMode() ? (
+          {/* {isDarkMode() ? (
             <Image className="d-block w-100" src="https://via.placeholder.com/800x200/222222/222222" alt="Carousel slide" />
           ) : (
             <Image className="d-block w-100" src="https://via.placeholder.com/800x200/ffffff/ffffff" alt="Carousel slide" />
-          )}
+          )} */}
           <Carousel.Caption>
             <h2>
               {t("carousel_question")} {index + 1}
